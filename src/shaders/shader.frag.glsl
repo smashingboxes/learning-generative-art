50b88afc6878a32d2984e6404c75b97a37afd016
Keegan Brown <brown.keegan@gmail.com>
2016-03-29 18:46:19 -0500

    Color variance

--- src/shaders/shader.frag.glsl
+++ src/shaders/shader.frag.glsl
@@ -10,11 +10,13 @@ uniform vec2 delayMouse;
 uniform vec2 resolution;
 
 const float timeMult = 1.5;
-const float radius = 4.;
-const int sections = 36;
-const float travelDist = 650.;
+const float radius = 40.;
+const int sections = 8*4;
+const float travelDist = 950.;
 const float hPI = 3.141592653589793 / 2.;
 const float seed = 1.;
+const float colorVariance = 1.;
+const float colorVarianceLow = 0.4;
 
 #define clamps(x) clamp(x,0.,1.)
 #define clampy(x) clamp(x,-0.2,0.8)
@@ -50,33 +52,43 @@ float drown (float val) {
 float distanceColorClamp (vec2 xyPos, vec2 center, float radius) {
   return clamps( (resolution.x - distance( center, xyPos ) + radius) / resolution.x );
 }
-vec3 getDotColor (vec2 loc, vec3 color, float radius, float index, vec2 mouseDiff, vec2 center) {
-  float dist = drown( distanceColorClamp(loc, center, radius) );
-  return vec3( clamps(color.r+dist), clamps(color.r+dist), clamps(color.r+dist) );
-}
 vec2 getRotatedDistance (float angle, vec2 mouseDiff, float dist, int i, vec2 center) {
   return vec2(
     //cos(-tick()+angle)*(dist*sin((time/10.)*float(i+1))),
     //sin(-tick()+angle)*(dist*sin((time/10.)*float(i+1)))
-    cos(tick()+angle)*(dist*(cos((float(i+1)*time/5.)+(rand(mouseDiff)*0.2)))),
-    sin(tick()+angle)*(dist*(cos((float(i+1)*time/5.)+(rand(mouseDiff)*0.2))))
+    cos(tick()+angle)*(dist*(cos((float(i+1)*time/5.)+(mouseDiff.x)))),
+    sin(tick()+angle)*(dist*(cos((float(i+1)*time/5.)+(mouseDiff.y))))
   ) + center;
+}
+vec3 getColorTweak ( float i ) {
+  if ( mod(i, 4.) <= 0.9 ) {
+    return vec3(colorVarianceLow,colorVarianceLow,colorVariance);
+  } else if ( mod(i, 3.) <= 0.9 ) {
+    return vec3(colorVarianceLow,colorVariance,colorVarianceLow);
+  } else if ( mod(i, 2.) <= 0.9 ) {
+    return vec3(colorVariance,colorVarianceLow,colorVarianceLow);
+  }
+  return vec3(0,0,0);
+}
+vec3 getDotColor (vec2 loc, vec3 color, float radius, float index, vec2 mouseDiff, vec2 center) {
+  float dist = drown( distanceColorClamp(loc, center, radius) );
+  return vec3( clamps(color.r+dist), clamps(color.g+dist), clamps(color.b+dist) ) * getColorTweak( float(index) );
 }
 void main() {
   vec2 mouseDiff = mouse - delayMouse;
   vec2 center = ( delayMouse ) * resolution;
-  vec3 c = vec3(0,0,0); //vec3( clampy(sin(time/1.)*0.13), clampy(sin(time/3.)*0.13), clampy(sin(time/8.)*0.13) );
+  vec3 baseColor = vec3(0,0,0); //vec3( clampy(sin(time/1.)*0.13), clampy(sin(time/3.)*0.13), clampy(sin(time/8.)*0.13) );
   float dist = (sin(time)*travelDist);
   float _radius = radius; //*((sin(time)*.5)+.5);
   vec3 finalColor = vec3(0,0,0);
 
-  finalColor = finalColor + getDotColor(gl_FragCoord.xy, c, _radius, float(0), mouseDiff, center);
+  finalColor = finalColor + getDotColor(gl_FragCoord.xy, baseColor, _radius, float(0), mouseDiff, center);
 
   for ( int i = 0; i < sections; i++ ) {
     float angle = (float(i)*hPI*4.)/float(sections);
     finalColor = finalColor + (getDotColor(
       gl_FragCoord.xy,
-      c,
+      baseColor,
       _radius,
       float(i),
       mouseDiff,
