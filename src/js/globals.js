e6229a918d6d18984d28a98dc7c3b2b07d2f0345
Keegan Brown <brown.keegan@gmail.com>
2016-09-19 12:09:12 -0500

    Fix syntax

--- src/js/globals.js
+++ src/js/globals.js
@@ -1,5 +1,5 @@
-let _ROOT = (location.origin.match(/(localhost|generative-artist)/gi) ?  location.origin : 'http://generative-artist.smashingboxes.com';
-const ROOT = (_ROOT.replace('8080','3210'));
+let _ROOT = location.origin.match(/(localhost|generative-artist)/gi) ?  location.origin : 'http://generative-artist.smashingboxes.com';
+const ROOT = _ROOT.replace('8080','3210');
 
 module.exports = {
   ROOT:ROOT
