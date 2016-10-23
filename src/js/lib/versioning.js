fd3844fedb978a3c84bf4eeb4b8ebdec5f0c4f01
Keegan Brown <brown.keegan@gmail.com>
2016-10-23 16:04:10 -0500

    Better versioning

--- src/js/lib/versioning.js
+++ src/js/lib/versioning.js
@@ -1,9 +1,11 @@
 import featureDetection from './featureDetection';
 
-let version = '1.0.1'; // bump this number if you want to reset locally stored items
+let version = '1.0.2'; // bump this number if you want to reset locally stored items
 export default (function versioning() {
-  if (!featureDetection.isSupportedDevice()) return 0;
-  //console.log('localStorage.version', localStorage.version)
+  if (!featureDetection.isSupportedDevice()) {
+    console.log('Djarvis does not support this device.');
+    return 0;
+  }
   if (localStorage.version) {
     if (localStorage.version !== version) {
       console.log('version bump. clear localStorage.', version);
@@ -11,6 +13,7 @@ export default (function versioning() {
       localStorage.setItem('version', version);
     }
   } else {
+    localStorage.clear();
     localStorage.setItem('version', version);
   }
 })();
